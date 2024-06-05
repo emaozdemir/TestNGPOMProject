4039bec9bfda576b379d6391ed1750da82cd3381
emaozdemir <semaozdemiraslan@gmail.com>
2024-06-05 17:33:04 +0200

    05.06

---
+++ src/test/java/clarusway/homework/Hw30.java
@@ -0,0 +1,56 @@
+package clarusway.homework;
+
+import clarusway.pages.HBBilgisayarlar;
+import clarusway.utilities.ConfigReader;
+import clarusway.utilities.Driver;
+import clarusway.utilities.JavascriptUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Hw30 {
+    /*
+    Given
+        Go to https://www.hepsiburada.com/bilgisayarlar-c-2147483646
+    When
+        Click all marks and print their names
+        (Soldaki markaların hepsine tıklayıp adlarını yazdırınız)
+    */
+
+    @Test
+    public void Hw30() throws InterruptedException {
+
+        //Driver.getDriver().get(ConfigReader.getProperty("hbbilgisayarlar_url"));
+        Driver.getDriver().get("https://www.hepsiburada.com/bilgisayarlar-c-2147483646");
+
+        HBBilgisayarlar hbBilgisayarlar = new HBBilgisayarlar();
+
+        List<WebElement> markalar = hbBilgisayarlar.markaElements; // Test classinda yeni bir list olusturulup markalar eklendi.
+
+
+        for (int i = 0; i < markalar.size(); i++) {
+
+            Thread.sleep(1000); // olmadan StaleElementReferenceException hatasi veriyor wait kullanilabilir.
+            //JavascriptUtils.scrollIntoViewJS(markalar.get(i));
+            /*
+            39. satirdaki kod yani JS scrollIntoViewJS() methodu kullanilirsa ne kadar marka varsa tek tek gorunur hale gelinceye kadar bekliyor sonrasinda click islemi yapiyor.
+            web sayfasinda kac tane marka varsa tek tek hepsini bulup click yapiyor ben 100 markaya kadar yazdirip durdurdum.
+            39. satir kapatilirsa ilk anda algiladigi 20 markayi alip click yapip yazdiriyor
+            */
+            JavascriptUtils.clickElementByJS(markalar.get(i)); // JS methodu ile click islemi yapiyor.
+
+            String marka = markalar.get(i).getAttribute("value"); // listteki herbir webelementin value degerini yani marka isimlerini bize getiriyor.
+            System.out.println((i + 1) + ". marka = " + marka); // markalari yazdiriyor.
+        }
+
+        Driver.closeDriver();
+    }
+}
---
+++ src/test/java/clarusway/pages/HBBilgisayarlar.java
@@ -0,0 +1,23 @@
+package clarusway.pages;
+
+import clarusway.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class HBBilgisayarlar {
+
+    public HBBilgisayarlar() {
+
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+    @FindBy(xpath = "//input[@name='markalar']")
+    public List<WebElement> markaElements;
+
+}
